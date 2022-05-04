b768463d102c326b1a2464710d793e59c9861796
Aen <eandur@gmail.com>
2022-05-03 23:40:18 -0300

    update ignores files without h

--- scripts/update_symlinks.bat
+++ scripts/update_symlinks.bat
@@ -5,11 +5,13 @@ CD src
 FOR %%I IN (*.cpp) DO (
     SET NAME=%%~nI.h
     IF NOT EXIST !NAME! (
-        MKLINK  !NAME! ..\include\!NAME!
+        IF EXIST ..\include\!NAME! (
+            MKLINK  !NAME! ..\include\!NAME!
+        ) ELSE (
+            ECHO Skipping %%I
+        )
     ) ELSE (
         ECHO Symlink up-to-date: !NAME! 
     )
 )
-
-
 
